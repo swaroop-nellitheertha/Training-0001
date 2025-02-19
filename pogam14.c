0444a95250e557019873bc766e2c6700a45e9b48
meoww <swaroopai1811@gmail.com>
2025-02-19 12:21:39 +0530

    Create pogam14.c

---
+++ pogam14.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Student{
+    char name[255];
+    int marks;
+};
+typedef struct Student Student_t;
+void readStudents(Student_t* students, int length){
+    for(int i=0;i<length;i++){
+        printf("Student %d Name:",i+1);
+        scanf("%s",students[i].name);
+        printf("Student marks:");
+        scanf("%d",&students[i].marks);
+    }
+}
+void printStudents(Student_t* students, int length){
+    printf("Student Records:\n");
+    for(int i=0;i<length;i++){
+        printf("%s - %d\n",students[i].name,students[i].marks);
+    }
+}
+int main(){
+    
+    int studentsCount;
+    printf("Enter number of students:");
+    scanf("%d",&studentsCount);
+    Student_t* students = (Student_t*)malloc(sizeof(Student_t)*studentsCount);
+    readStudents(students,studentsCount);
+    printStudents(students, studentsCount);
+    free(students);
+    students=NULL;
+    return 0;
+}
